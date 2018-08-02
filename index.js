a7a2dbae6d4335944791e51d65d5d0d0c688e14a
JadeSpirit <dwstar@hotmail.com>
2018-08-02 13:08:44 +0300

    Working export to csv

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var Zabbix = require ('Zabbix');
+var Zabbix = require ('zabbix');
 var xlsx = require ('node-xlsx').default;
 var json2array = require("json2array");
 var jsonexport = require("jsonexport");
 var writeFileSync = require ('jest-serializer');
+var fs = require ('fs');
 
 var zabbix = new Zabbix('http://10.82.252.68/api_jsonrpc.php','Admin', 'JetZabbixAdmin');
 
@@ -14,17 +15,24 @@ zabbix.getApiVersion(function (err, resp, body) {
 zabbix.login(function (err, resp, body) {
   if (!err) {
     console.log("Authenticated! AuthID is: " + zabbix.authid);
-  }});
+  }
 
   zabbix.call("template.get",
     {
-    "search" : {"host" : "JET critical files audit"},
+    "search" : {"host" : "JET Linux"},
     "output" : "extend",
     "selectItems" : "extend"
     }
     ,function (err, resp, body) {
       if (!err) {
         var data = (body.result[0]);
-        const myFile = "C:\Users\nikitchenkovy\Documents\zbx.json";
-        writeFileSync(myFile, data);
-      }});
+        jsonexport(data,function(err, csv){
+          if(err) return console.log(err);
+          console.log("Writing current template to out.csv");
+          fs.writeFile('out.csv', csv, function(err) {
+          if (err) console.log(err);
+        })
+        });
+      }
+    });
+  });
