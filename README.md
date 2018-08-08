710acc8b9b5f8eb83957d056ccfe217878ed9929
JadeSpirit <dwstar@hotmail.com>
2018-08-08 22:00:16 +0300

    Code refactor, all export working now

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ Usage.
 Downloading template:
 node download.js --hostname=Your zabbix hostname --username=Zabbix admin username --password=Zabbix admin password --template="Template to download" --format="xml|json"
 Expected result: exported template  in form of "template".xml or "template".json files in root folder.
+
+XML is supported for importing via zabbix web interface.
+JSON is required for future import via upload.js or for other needs.
+
+Uploading template:
+|||||placeholder|||||
+
+
+TODO:
+Create methods and index.js
--- download.js
+++ download.js
@@ -6,8 +6,8 @@ var Zabbix = require('zabbix');
 var fs = require('fs');
 const { toXML } = require('jstoxml');
 
-var apiversion = "0";
-
+var apiversion = undefined;
+var templateid = undefined;
 var template = argv.template;
 var format = argv.format;
 
@@ -21,53 +21,53 @@ var gettemplate = {
 };
 
 var zabbix = new Zabbix('http://' + argv.hostname + '/api_jsonrpc.php', argv.username, argv.password);
-
 console.log("Auth string is " + JSON.stringify(zabbix));
 
-zabbix.getApiVersion(function (err, resp, version) {
-  if (!err) {
-  console.log("Zabbix api version is " + version.result);
-  apiversion = (version.result);
-}
-if (err) {
-  console.log (err);
-}
-});
+  zabbix.getApiVersion(function (err, resp, version) {
+    if (!err) {
+        console.log("Zabbix api version is " + version.result);
+        apiversion = (version.result);
+      }
+  if (err) {
+    console.log (err);
+    }
+  });
+
 zabbix.login(function (err, resp, body) {
   if (!err) {
     console.log("Authenticated! AuthID is: " + zabbix.authid);
+    zabbix.call("template.get", gettemplate, function (err, resp, body) {
+      if (!err) {
+        var temp = JSON.parse(JSON.stringify(body.result[0]));
+        templateid = (temp.templateid);
+        console.log ("Template ID is: " + templateid);
+        var getdata = {
+          "options" : {
+            "templates" : [
+              templateid
+            ]
+          },
+          "format": format
+        };
+        console.log("Aquiring template using " + JSON.stringify(gettemplate) + JSON.stringify(getdata));
+        zabbix.call("configuration.export", getdata, function (err, resp, body) {
+          if (!err) {
+            data = (body.result);
+            fs.writeFile(template + '.'+ format, data, function (err) {
+              if (err) console.log(err);
+            });
+          }
+          if (err) {
+            console.log(err);
+          }
+        });
+      }
+      if (err) {
+        console.log (err)
+      }
+  })
   }
-
-  zabbix.call("template.get", gettemplate, function (err, resp, body) {
-    if (!err) {
-      console.log (body.result[0]);
-    }
-    if (err) {
-      console.log (err);
-    }
-});
-
-  var getdata = {
-    "options" : {
-      "templates" : [
-        "10170"
-      ]
-    },
-    "format": format
-  };
-
-  console.log("Aquiring template using " + JSON.stringify(gettemplate) + JSON.stringify(getdata));
-
-  zabbix.call("configuration.export", getdata, function (err, resp, body) {
-    if (!err) {
-      console.log (body.result);
-      data = (body.result);
-      fs.writeFile(template + '.'+ format, data, function (err) {
-        if (err) console.log(err);
-      });
-    }
-    if (err) {
-      console.log(err);
-    }
-  });
+  if (err) {
+    console.log (err);
+  }
 });
